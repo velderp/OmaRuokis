0e0d838c0155a612a077c4332ba975729edad0c0
Juhah <Juha.Rasanen2@metropolia.fi>
2019-03-11 14:54:08 +0200

    Added activity_info

--- app/src/main/java/com/example/omaruokis/utilities/BodyCalc.java
+++ app/src/main/java/com/example/omaruokis/utilities/BodyCalc.java
@@ -7,6 +7,16 @@ public class BodyCalc {
     private String sex;
     private double activityLevel;
 
+    /**
+     * Constructs and initializes weight, height, sex, activityLevel.
+     * @param weight amount
+     * @param height height
+     * @param sex defines male or female
+     * @param activityLevel defines PAL, physical activity level
+     * @param dateOfBirth Personal date of birth
+     * @param date date
+     */
+
     public BodyCalc(int weight, int height, String sex, double activityLevel,
                     String dateOfBirth, String date) {
         this.weight = weight;
@@ -16,17 +26,40 @@ public class BodyCalc {
         this.age = calcAge(dateOfBirth, date);
     }
 
+    /**
+     *
+     * @return returns BMR, Basal metabolic rate
+     */
+
     private int calcBmr(){
         int sex = (this.sex.equals("M")) ? 5 : -161;
         return (int) (10 * this.weight + 6.25 * this.height - 5 * this.age + sex);
     }
+
+    /**
+     *
+     * @return returns total energy expenditure
+     */
+
     public int calcTee(){
         return (int) (calcBmr() * this.activityLevel);
     }
 
+    /**
+     *
+     * @return returns body mass index
+     */
+
     public double calcBmi(){
         return this.weight / ((this.height / 100.0) * (this.height / 100.0));
     }
+
+    /**
+     *
+     * @param dateOfBirth date of birth set by user
+     * @param date date today
+     * @return returns age using calendar
+     */
 
     private int calcAge(String dateOfBirth, String date){
         String[] partsDOB = dateOfBirth.split("\\.");
